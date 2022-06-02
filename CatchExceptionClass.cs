052f552587a43bfb94330526c23f1af75acd4f72
rootrequired <104975826+rootrequired@users.noreply.github.com>
2022-06-02 22:09:22 +0300

    Add files via upload

---
+++ CatchExceptionClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Main;
+
+namespace CSharp_Net_module1_3_1_lab
+{
+    class CatchExceptionClass
+    {
+        public void CatchExceptionMethod()
+        {
+            try
+            {
+                MyArray ma = new MyArray();
+                int[] arr = new int[4] { 1, 4, 8, 5 };
+                arr[1] = 0;
+                ma.Assign(arr, 4);
+                arr = null;
+                ma.Assign(arr, 5);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Exception : " + ex.Message);
+                Console.WriteLine("Stack Trace : " + ex.StackTrace);
+                Console.WriteLine("Source : " + ex.Source);
+                Console.WriteLine("HelpLink : " + ex.HelpLink);
+                Console.WriteLine("TargetSite : " + ex.TargetSite);
+            }
+            finally
+            {
+                Console.WriteLine("Finally, block which will be always showed )");
+            }
+        }
+    }
+}
---
+++ MyArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_3_1_lab
+{
+    class MyArray
+    {
+        int[] arr;
+
+        public void Assign(int[] arr, int size)
+        {
+            try
+            {
+                this.arr[0] = 5;
+                try
+                {
+                    this.arr = new int[size];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        this.arr[arr.Length + 1] = 5;
+                        this.arr[i] = arr[i] / arr[i + 1];
+                    }
+                    unchecked
+                    {
+                        arr[arr.Length - 1] = 1000000000 * 100;
+                    }
+                    arr = null;
+                    arr[0] = 5;
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Exception : " + ex.Message);
+                    Console.WriteLine("StackTrace : " + ex.StackTrace);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can't make division by zero\nException : " + ex.Message);
+                Console.WriteLine("StackTrace : " + ex.StackTrace);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Operation with null objects aren't allowed\nException : " + ex.Message);
+                Console.WriteLine("StackTrace : " + ex.StackTrace);
+            }
+        }
+    }
+}
